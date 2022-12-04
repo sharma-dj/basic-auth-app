c085ea2d1139aaad5ca1991ee06b546a25c9e224
DJ <dj4388>
2022-12-04 23:58:12 +0530

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,7 @@ const App = () => {
         <nav id='menu'>
           <ul>
             <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
+              <Link to="/">Dashboard</Link>
             </li>
             <li>
               <Link to="/register">Register</Link>

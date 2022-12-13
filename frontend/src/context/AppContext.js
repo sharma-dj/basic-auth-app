748f740e55261659e9137081fa7d49bf317863d9
DJ <dj4388@gmail.com>
2022-12-13 23:30:26 +0530

    updates into appcontext

--- frontend/src/context/AppContext.js
+++ frontend/src/context/AppContext.js
@@ -1,5 +1,6 @@
 import React,{useState,useReducer,useContext,createContext} from 'react'
 import Reducer from './Reducer';
+import { REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR, LOGIN_USER_BEGIN, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR } from "./Actions"
 
 const initialState = {
   user:null,
